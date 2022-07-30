3fe9035ebd7c19a41db8b4c75133875024988669
kamranMaqbool <kamranaaqbool37@gmail.com>
2022-07-30 17:59:41 +0500

    implement ordering filters

--- store/views.py
+++ store/views.py
@@ -9,7 +9,7 @@ from rest_framework.mixins import CreateModelMixin, ListModelMixin
 from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.views import APIView
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 from .filters import ProductFilter
 
@@ -22,9 +22,10 @@ from .serializers import CollectionSerializer, ProductsSerializer, ReviewSeriali
 class ProductViewset(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductsSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
     search_fields = ['title', 'description']
+    ordering_fields = ['unit_price', 'last_update']
 
     def destroy(self, request, *args, **kwargs):
         if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
